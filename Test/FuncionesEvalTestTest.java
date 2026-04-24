d497520154f21650a520c2f509e1e6b7945d50ad
PedroJose20 <nunez.lajos25@cordoba.salesianos.edu>
2026-04-24 16:36:57 +0200

    Test de las funciones esPerfecto, getTipoClima e invertirArray hechos

--- Test/FuncionesEvalTestTest.java
+++ Test/FuncionesEvalTestTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,13 +7,26 @@ class FuncionesEvalTestTest {
 
     @Test
     void esPerfecto() {
+        assertTrue(FuncionesEvalTest.esPerfecto(6));
+        assertFalse(FuncionesEvalTest.esPerfecto(7));
+        Assertions.assertThrows(ArithmeticException.class, () -> FuncionesEvalTest.esPerfecto(0));
     }
 
     @Test
     void getTipoClima() {
+    assertEquals("FRIO", FuncionesEvalTest.getTipoClima(5));
+    assertEquals("NUBLADO", FuncionesEvalTest.getTipoClima(14));
+        assertEquals("NUBLADO", FuncionesEvalTest.getTipoClima(20));
+    assertEquals("CALUROSO", FuncionesEvalTest.getTipoClima(25));
+        assertEquals("CALUROSO", FuncionesEvalTest.getTipoClima(30));
+    assertEquals("TROPICAL", FuncionesEvalTest.getTipoClima(37));
+    assertEquals("FRIO", FuncionesEvalTest.getTipoClima(-100));
     }
 
     @Test
     void invertirArray() {
+    assertArrayEquals(new int[]{3, 2, 1}, FuncionesEvalTest.invertirArray(new int[]{1, 2, 3}));
+    Assertions.assertThrows(IllegalArgumentException.class, () -> FuncionesEvalTest.invertirArray(null));
+
     }
 }
