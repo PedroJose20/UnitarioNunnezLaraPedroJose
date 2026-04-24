0eed75a0eb201866dfba6adc9b14462d01a94a08
PedroJose20 <nunez.lajos25@cordoba.salesianos.edu>
2026-04-24 16:15:20 +0200

    Creacion del proyecto y creacion de la funcion y la carpeta donde hacer los tests

---
+++ Test/FuncionesEvalTestTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionesEvalTestTest {
+
+    @Test
+    void esPerfecto() {
+    }
+
+    @Test
+    void getTipoClima() {
+    }
+
+    @Test
+    void invertirArray() {
+    }
+}
